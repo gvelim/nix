513c5db55938df4a10f273eddc413633e6b82314
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-10-18 13:15:04 +0100

    added nvim with nix lsp

--- flake.nix
+++ flake.nix
@@ -2,20 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-  };
+    nixvim = {
+    	url = "github:nix-community/nixvim";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
+ };
 
-  outputs = { self, nixpkgs, flake-utils } : 
+  outputs = { self, nixpkgs, flake-utils, nixvim } : 
     flake-utils.lib.eachDefaultSystem (system :
       let 
         pkgs = nixpkgs.legacyPackages.${system};
         name = "simple";
         src = ./.;
+	nvim = nixvim.legacyPackages.${system}.makeNixvim {
+	  plugins.lsp.enable = true;
+	  plugins.lsp.servers.nixd = {
+	  	enable = true;
+    		settings = {
+        	  options.nixvim.expr = ''(builtins.getFlake "/path/to/flake").packages.${system}.neovimNixvim.options'';
+    		};
+	  };
+	};
       in {
         packages.default = derivation {
             inherit system name src;
             builder = with pkgs; "${bash}/bin/bash";
             args = [ "-c" "echo foo > $out" ];
           };
+	devShells.default = with pkgs ; mkShell {
+	  buildInputs = [
+	  	nixd 
+		nvim
+	  ];
+	};
       }
     );
 }
