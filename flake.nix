c1e75c389f7a856498afa39fd74f95a9afb3373d
george velimachitis <3185416+gvelim@users.noreply.github.com>
2024-10-18 17:16:58 +0100

    shell.nix

--- flake.nix
+++ flake.nix
@@ -10,52 +10,67 @@
 
   outputs = { self, nixpkgs, flake-utils, nixvim } : 
     flake-utils.lib.eachDefaultSystem (system :
-      let 
-        pkgs = nixpkgs.legacyPackages.${system};
-        name = "simple";
-        src = ./.;
-	nvim = nixvim.legacyPackages.${system}.makeNixvim {
-	plugins.treesitter = {
-	  enable = true;
-	  settings = {
-	     highlight.enable = true;
-	     auto_install = true;
-	  };
-	  grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-	  bash
-          json
-          lua
-          make
-          markdown
-          nix
-          regex
-          toml
-          vim
-          vimdoc
-          xml
-          yaml
-	  ];
-	  };
-	  plugins.lsp.enable = true;
-	  plugins.lsp.servers.nixd = {
-	  	enable = true;
-    		settings = {
-        	  options.nixvim.expr = ''(builtins.getFlake "/path/to/flake").packages.${system}.neovimNixvim.options'';
-    		};
-	  };
-	};
-      in {
-        packages.default = derivation {
-            inherit system name src;
-            builder = with pkgs; "${bash}/bin/bash";
-            args = [ "-c" "echo foo > $out" ];
-          };
-	devShells.default = with pkgs ; mkShell {
-	  buildInputs = [
-	  	nixd 
-		nvim
-	  ];
-	};
-      }
-    );
+    	let 
+			pkgs = nixpkgs.legacyPackages.${system};
+			name = "simple";
+			src = ./.;
+			nvim = nixvim.legacyPackages.${system}.makeNixvim {
+				plugins.treesitter-context = {
+					enable = true;
+					settings = {
+						max_lines = 4;
+					};
+				};
+				plugins.treesitter = {
+					enable = true;
+					settings = {
+						auto_install = true;
+						indent.enable = true;
+						incremental_selection.enable = true;
+						highlight = {
+							enable = true;
+							disable = ''
+								function(lang, bufnr)
+									return vim.api.nvim_buf_line_count(bufnr) > 10000
+								end
+							'';
+						};
+					};
+					grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+						bash
+						json
+						lua
+						make
+						markdown
+						nix
+						regex
+						toml
+						vim
+						vimdoc
+						xml
+						yaml
+					];
+				};
+				plugins.lsp.enable = true;
+				plugins.lsp.servers.nixd = {
+					enable = true;
+					#settings = {
+					#	options.nixvim.expr = ''(builtins.getFlake "/path/to/flake").packages.${system}.neovimNixvim.options'';
+					#};
+				};
+			};
+		in {
+			packages.default = derivation {
+				inherit system name src;
+				builder = with pkgs; "${bash}/bin/bash";
+				args = [ "-c" "echo foo > $out" ];
+			};
+			devShells.default = with pkgs ; mkShell {
+				buildInputs = [
+					nixd 
+					nvim
+				];
+			};
+		}
+	);
 }
